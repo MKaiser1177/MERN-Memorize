46cebf03a18f25864401a86e3d40823b41fd7e01
Vedant Agrawal <2vedantagrawal@gmail.com>
2026-02-24 19:47:49 +0530

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import react from 'react';
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "api"
+
 const api= axios.create({
     baseURL: 'http://localhost:5001/api',
 })
