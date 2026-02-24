18c1d4be70983f6435285167f23460c2259ea4c8
Vedant Agrawal <2vedantagrawal@gmail.com>
2026-02-24 20:11:06 +0530

    Updated server.js to serve static files

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,10 +29,10 @@ app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(process.cwd(), "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.join(process.cwd(), "../frontend/dist/index.html"));
 
 });
 
